a8eb23f6e885d7f14110565ce6b90d83a755759a
zachschultz <zacharymschultz@gmail.com>
2018-01-22 20:14:08 -0600

    adding fzf and more config

--- zsh/entry.sh
+++ zsh/entry.sh
@@ -10,10 +10,13 @@ source $DOTFILES/zsh/aliases.sh
 # Setup dircolors for LS
 eval `dircolors $DOTFILES/zsh/dircolors`
 
-# Include plugins
+#################################################
+# Plugins 
+################################################
 source $INCLUDES/zsh-completions/zsh-completions.plugin.zsh
 source $INCLUDES/zsh-history-substring-search/zsh-history-substring-search.zsh
 source $INCLUDES/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $HOME/.fzf.zsh
 
 # Setup history options
 HISTFILE=$HOME/.zsh_history
@@ -30,5 +33,35 @@ if [[ $machine == "Linux" ]]; then
   source ~/.dotfiles/linuxbrew/exports.sh
 fi
 
+################################################
+# ZSH configurations
+################################################
+zstyle ':completion:*' menu select
+zstyle ':completion:*' completer _complete
+zstyle ':completion:*' matcher-list '' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' '+l:|=* r:|=*'
 
+# Enable zsh-completions
+autoload -U compinit && compinit
+zmodload -i zsh/complist
 
+unsetopt menu_complete
+unsetopt flowcontrol
+
+setopt prompt_subst
+setopt always_to_end
+setopt append_history
+setopt auto_menu
+setopt complete_in_word
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_verify
+setopt inc_append_history
+setopt interactivecomments
+setopt share_history
+
+################################################
+# Prompt
+################################################
+source $DOTFILES/zsh/prompt.sh
---
+++ zsh/prompt.sh
@@ -0,0 +1,17 @@
+# Prompt
+git_prompt() {
+  BRANCH=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/*\(.*\)/\1/')
+
+  if [ ! -z $BRANCH ]; then
+    echo -n "%F{yellow}$BRANCH"
+
+    if [ ! -z "$(git status --short)" ]; then
+      echo " %F{red}✗"
+    fi
+  fi
+}
+
+PS1='
+%F{blue}%~$(git_prompt)
+%F{244}%# %F{reset}'
+
