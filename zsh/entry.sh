7b96113c5a99e61ccc2de2dde68058f30c14bf01
zachschultz <zacharymschultz@gmail.com>
2018-01-22 19:43:38 -0600

    updating install and other files to include plugins

--- zsh/entry.sh
+++ zsh/entry.sh
@@ -1,11 +1,34 @@
-# Get exports from linuxbrew config
-# @TODO make this specific to Linux distros
-source ~/.dotfiles/linuxbrew/exports.sh
+#!/usr/bin/env zsh
 
-# ##################################################################################################
-# Aliases
-# ##################################################################################################
+export DOTFILES=$HOME/.dotfiles
+export INCLUDES=$HOME/.local/share/dotfiles
 
-source ~/.dotfiles/zsh/aliases.sh
+# Include environment
+source $DOTFILES/zsh/env.sh
+# Include aliases
+source $DOTFILES/zsh/aliases.sh
+# Setup dircolors for LS
+eval `dircolors $DOTFILES/zsh/dircolors`
+
+# Include plugins
+source $INCLUDES/zsh-completions/zsh-completions.plugin.zsh
+source $INCLUDES/zsh-history-substring-search/zsh-history-substring-search.zsh
+source $INCLUDES/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# Setup history options
+HISTFILE=$HOME/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+
+# If on Linux, include exports from linuxbrew config
+os="$(uname -s)"
+case "${os}" in
+    Linux*)     machine=Linux;;
+    *)          machine="UNKNOWN:${os}"
+esac 
+if [[ $machine == "Linux" ]]; then
+  source ~/.dotfiles/linuxbrew/exports.sh
+fi
+
 
 
