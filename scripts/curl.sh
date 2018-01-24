10a311d122987b47c3d5c6c4d28a85cbf595a869
zachschultz <zacharymschultz@gmail.com>
2018-01-24 13:16:46 -0600

    installer will install software now too

--- scripts/curl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,36 +1,13 @@
 #!/bin/bash
-# symlink dotfiles
-set -x
-lsrc -x README.md -x scripts -x zsh -x linuxbrew -v 
-echo 'About to symlink dotfiles, does this look ok? [Y/n]'
-output=$( lsrc -x README.md -x scripts -x zsh -x linuxbrew -v )
-echo "$output"
-# suppress welcome message
-if [ ! -f $HOME/.hushlogin ]; then
-  touch $HOME/.hushlogin
-fi
 
-os="$(uname -s)"
-case "${os}" in
-    Linux*)     machine=Linux;;
-    Darwin*)    machine=Mac;;
-    CYGWIN*)    machine=Cygwin;;
-    MINGW*)     machine=MinGw;;
-    *)          machine="UNKNOWN:${os}"
-esac
-
-if [ "${machine}" == "Linux" ]; then
+if [ "${MACHINE}" == "Linux" ]; then
   # install with linuxbrew
   source ~/.dotfiles/scripts/linuxbrew.sh
-elif [ "${machine}" == "Darwin" ]; then
+elif [ "${MACHINE}" == "Mac" ]; then
   # install with brew
   source ~/.dotfiles/scripts/brew.sh
 fi
 
 # install atom packages
 source ~/.dotfiles/scripts/apm.sh
-
-# install with curl
-source ~/.dotfiles/scripts/curl.sh
-
 
